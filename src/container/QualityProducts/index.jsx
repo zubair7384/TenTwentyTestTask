14405d05d8ab2594eb17128847dc8499ea9fc023
zubair7384 <zubair.iqbal2018@gmail.com>
2023-03-01 23:31:24 +0400

    fixed some issues

--- src/container/QualityProducts/index.jsx
+++ src/container/QualityProducts/index.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import ProductSlider from "../../components/ProductSlider";
-import Wheel from "../../components/ProductSlider";
 import ProductOne from "../../assets/images/product-1.jpg";
 import ProductThree from "../../assets/images/product-3.jpg";
 import "./qualityProducts.scss";
@@ -15,9 +14,8 @@ const QualityProducts = () => {
     dragSpeed: 0.4,
     itemWidth: 280,
     itemHeight: 657,
-    itemSideOffsets: 150,
+    itemSideOffsets: 90,
   };
-
   const itemStyle = {
     width: `${setting.itemWidth}px`,
     height: `${setting.itemHeight}px`,
@@ -27,20 +25,29 @@ const QualityProducts = () => {
   return (
     <div className="quality-products">
       <div className="quality-products-content">
-        <h2
-          className={`animate-title ${titleAnimated ? "animated" : ""}`}
-          onAnimationEnd={() => setTitleAnimated(true)}
-        >
-          Quality Products
-        </h2>
-        <p className={`animate-paragraph ${titleAnimated ? "animated" : ""}`}>
+        <h2>Quality Products</h2>
+        <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat.
         </p>
       </div>
-      <ProductSlider />
+      <ProductSlider
+        data={slides}
+        {...setting}
+        activeSlideIndex={activeSlideIndex}
+      >
+        {slides.map((slide, index) => (
+          <div
+            key={index}
+            className={`item ${index === activeSlideIndex ? "active" : ""}`}
+            style={{ ...itemStyle }}
+          >
+            <img src={slide} alt="slider-image" />
+          </div>
+        ))}
+      </ProductSlider>
     </div>
   );
 };
